ec47b82efcfa0f6fec921cb7d53b60cf64c30b35
DevFrog92 <yongsu1481@gmail.com>
2022-01-05 16:54:35 +0900

    add: vue common components design patterns

---
+++ src/components/common/ItemList.vue
@@ -0,0 +1,22 @@
+<template>
+	<ul>
+		<li v-for="(item, index) in items" :key="index">
+			{{ item }}
+		</li>
+		<button @click="$emit('renew')">renew</button>
+	</ul>
+</template>
+
+<script>
+export default {
+	props: {
+		// props validation
+		items: {
+			type: Array,
+			required: true,
+		},
+	},
+};
+</script>
+
+<style></style>
