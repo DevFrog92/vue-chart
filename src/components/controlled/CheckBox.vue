abb7ab8e141da520bfe83fd17c4e07bdac52cd98
DevFrog92 <yongsu1481@gmail.com>
2022-01-05 17:25:43 +0900

    add vue controlled component design patterns

---
+++ src/components/controlled/CheckBox.vue
@@ -0,0 +1,21 @@
+<template>
+	<!-- NOTE: n:n 통신을 방지한다. -->
+	<input type="checkbox" :value="value" @click="toggleCheckBox" />
+</template>
+
+<script>
+export default {
+	props: {
+		value: {
+			type: Boolean,
+		},
+	},
+	methods: {
+		toggleCheckBox() {
+			this.$emit('input', !this.value);
+		},
+	},
+};
+</script>
+
+<style></style>
